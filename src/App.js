4f0f397c1ca0b9d2355becbdcc4b18ae681f061f
pidor <yu.nailka.rav@yandex.ru>
2022-05-31 08:07:10 +0500

    31

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import React from 'react';
+
 import Header from './components/Header';
 import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
+//import pizzas from './assets/pizzas.json';
 
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  // https://62948189a7203b3ed06a58de.mockapi.io/Items - пиццы тут
+
+  /* Проба через fetch, но так не надо
+  // fetch запрос - стандартная функция браузера. Запрашиваем данные пицц с mockapi.io.
+  fetch('https://62948189a7203b3ed06a58de.mockapi.io/Items') // Отправь запрос по адресу
+    .then((res) => {
+      // Дождались ответ, получили какое-то говно
+      return res.json(); // Конвертировали его в json
+    })
+    .then((arr) => {
+      setItems(arr);
+    });*/
+
+  //Выполняется только один раз при пустом массиве (второй параметр)
+  React.useEffect(() => {
+    fetch('https://62948189a7203b3ed06a58de.mockapi.io/Items') // Отправь запрос по адресу
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,7 +43,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
                 key={obj.id}
                 {...obj}
