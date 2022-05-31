8a273dfeea7532dee957bfe09fdd96e30f9e6b7d
pidor <yu.nailka.rav@yandex.ru>
2022-05-31 16:55:34 +0500

    Начало корзины

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
+
 import Home from './pages/Home';
+import Card from './pages/Card';
 import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
@@ -12,7 +15,11 @@ function App() {
             <Header />
             <div className="content">
                 <div className="container">
-                    <NotFound />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/card" element={<Card />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
                 </div>
             </div>
         </div>
---
+++ src/pages/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <>
+            <h1>Корзина</h1>
+        </>
+    );
+};
+
+export default Card;
